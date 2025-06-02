667e07e763584e764a5836ebdf6eb43fa4faf757
ranggarap <fadhilahrangga26@gmail.com>
2025-06-02 18:13:15 +0700

    method private di member x dan y

--- Abstraksi.cpp
+++ Abstraksi.cpp
@@ -4,4 +4,11 @@ using namespace std;
 class AbstraksiKlas
 {
     private:
-};
+        string x,y;
+
+public :
+        // method untuk mengisi nilai 
+        // private member
+        void setXY(string a, string b)
+        
+    };
